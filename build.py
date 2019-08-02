3996e0c76854e4c4791284974146d6042d926ff6
Re4son <3520622-re4son@users.noreply.gitlab.com>
2019-08-02 19:13:35 +1000

    Replace 3rd party apps with store app

--- build.py
+++ build.py
@@ -21,26 +21,14 @@ dl_supersu = {
 }
 
 dl_apps = {
-	'cSploit':
-		['https://github.com/cSploit/android/releases/download/v1.6.6-rc.2/cSploit-release.apk', 'b841c4376836bcc9d23fbc18b40eed70e08018e8eebc6d2d0abad59da63e4b325ffe4d8a4bd36107af63ed20a59c6648d6c4bd1264044267c86693744b15fa75'],
-	'Drivedroid':
-		['https://drivedroid-versions.s3-eu-west-1.amazonaws.com/files/drivedroid-free-0.10.50.apk', '455354369d34dd59cdf71e50c02fd1dc969925f803fbc1c498467ac1b3b50b7a3b23dd22a3e60a5dbe61d7106c99106f92ef6a86c56775f01e42dfcd1837c198'],
-	'Hackerskeyboard':
-		['https://f-droid.org/repo/org.pocketworkstation.pckeyboard_1039003.apk', '8c861c7540e6eeb006070d0f2d80134e75637066591d705b987e164c3fe87521ed694ac844c945eb74449aff8723ff039f793b2e3743aac73865f74bb248edf5'],
-	'Hijacker':
-		['https://github.com/chrisk44/Hijacker/releases/download/v1.5-beta.8/Hijacker-release-v1.5-beta.8.apk', 'f9aad94faa8a78833dcc1da2702455fe106383a95cc46a25559ff398e3adb828828c5545d8754d982511a2718ea842c56c7ee8566c5973c65d88858e5b1288c1'],
-	'Intercepter-NG':
-		['https://github.com/intercepter-ng/intercepter-ng.github.io/raw/master/Intercepter-NG.2.1.apk', '66a12b8ff1022635071283acbbd4f41bceae092cfdc88cbc0f0c77a175c8f5ba5d7fc1703474e7ea6c24b5630b6c2c970ae62641f7924ec47b72956f47afd979'],
-	'OpenVPN':
-		['https://f-droid.org/repo/de.blinkt.openvpn_159.apk', 'a47200c972a6e98649f6a8f87e02c5b924b1c7915ef86698f1339a1ec82af3eab82e7ecf442a3b2f5ab5ded91b59b0943315133413b50a2c473d5f9ba8050695'],
-	'RFAnalyzer':
-		['https://github.com/demantz/RFAnalyzer/raw/version_1_13/RFAnalyzer.apk', '7793438b6fbe7288a0ca86de900f5f4e607168de8c97229d08d901c2424b0192bf9dc894f66439f59510c10fa26a26319a1b0d8ea276f6af927cebf677138230'],
-	'RouterKeygen':
-		['https://github.com/routerkeygen/routerkeygenAndroid/releases/download/v4.0.2/routerkeygen-4-0-2.apk', '0135bbeb371f616c3577e6932e67b6c22c5aff335c7d7570bf5d26f106eadb0cbf4eea12503726cb806d18794e1adc6f3cc934ea41ca5ef62a8c77a4702947da'],
-	'Shodan':
-		['https://github.com/PaulSec/Shodan.io-mobile-app/raw/v0.0.3-new/io.shodan.app.apk', 'a2ff39d8e7a86d8e0a14368fd278fb03212999b309bc102d39f76ff69ca2a373d3d62a95cea6dbee761ae81ff3daaf83846e49e8ccbf0760276d825493d08652'],
-	'USBKeyboard': # Feb 3, 2015
-		['https://github.com/pelya/android-keyboard-gadget/raw/7ea69c684aa1/USB-Keyboard.apk', '18bced7b339a67c48fe31698cb54063bce8f3dd9f7d7f23d9e5c619697e8da5ab08312cf9a2fa0e3f445a584485db23d1e4c27e3ffc1448551bbaf486ccb11e9'],
+	'NetHunterStore':
+                ['https://store.nethunter.com/NetHunterStore.apk', 'aba5e5b6e09db80e7a2b42c2271cba304b4ce588ac607319ad6ce1f8bad13e9a7be40ed3a66aac91b735cd43827b1e0eda6b3811f541c3a00c962dbf50c159ef'],
+	'NetHunter':
+                ['https://store.nethunter.com/NetHunter.apk', 'f2347b57fc1f06f6cadb9f4cfe241496e1119c613f01b41b8381337ec6e3848580f6d40f4f27d8ec99db396666be86825ed1fb0af72d3aa6690a30be7c4ec243'],
+	'NetHunterTerminal':
+		['https://store.nethunter.com/NetHunterTerminal.apk', 'c6f47ed62ce15985ce3bcb7edcaaad29d6a200e44f1cb5a72e9a970937bca4236d72e934fcd5d7d08846f703a83e1ad0d7c8580cd9b54aa02a42e12c721aedde'],
+	'NetHunterVNC':
+		['https://store.nethunter.com/NetHunterVNC.apk', '1f81a85a040ddfedc98a6fcdecb5ef9153372c40b829fe12cbf2dd22cc1491ce1b6c4444ff8f865d124575e8315702686aef4a1d88985995581bc264fee61989'],
 }
 
 def copytree(src, dst):
