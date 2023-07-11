63b620fde4655a4e860cba344f5a49a007398a72
yesimxev <yesimxev@gitlab.com>
2023-07-11 09:46:51 +0100

    Updated to latest Anykernel3

--- boot-patcher/anykernel.sh
+++ boot-patcher/anykernel.sh
@@ -1,36 +1,23 @@
-# AnyKernel3 Ramdisk Mod Script
-# osm0sis @ xda-developers
+### AnyKernel3 Ramdisk Mod Script
+## osm0sis @ xda-developers
 
-# Customized for NetHunter
-
-## AnyKernel setup
+### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=
-do.devicecheck=0 #Use value 1 while using boot-patcher standalone
+kernel.string=ExampleKernel by osm0sis @ xda-developers
+do.devicecheck=1
 do.modules=0
-do.systemless=0 #Never use this for NetHunter kernels as it prevents us from writing to /lib/modules
-do.cleanup=0
+do.systemless=1
+do.cleanup=1
 do.cleanuponabort=0
-device.name1=
-device.name2=
-device.name3=
-device.name4=
+device.name1=maguro
+device.name2=toro
+device.name3=toroplus
+device.name4=tuna
 device.name5=
-device.name6=
-device.name7=
-device.name8=
-device.name9=
-device.name10=
-device.name11=
-device.name12=
-device.name13=
-device.name14=
-device.name15=
 supported.versions=
+supported.patchlevels=
 '; } # end properties
-
-# shell variables
 
 # NetHunter Addition
 ramdisk_compression=auto;
@@ -46,62 +33,62 @@ SYSTEM="/system";
 SYSTEM_ROOT="/system_root";
 
 setperm() {
-	find "$3" -type d -exec chmod "$1" {} \;
-	find "$3" -type f -exec chmod "$2" {} \;
+        find "$3" -type d -exec chmod "$1" {} \;
+        find "$3" -type f -exec chmod "$2" {} \;
 }
 
 install() {
-	setperm "$2" "$3" "$home$1";
-	if [ "$4" ]; then
-		cp -r "$home$1" "$(dirname "$4")/";
-		return;
-	fi;
-	cp -r "$home$1" "$(dirname "$1")/";
+        setperm "$2" "$3" "$home$1";
+        if [ "$4" ]; then
+                cp -r "$home$1" "$(dirname "$4")/";
+                return;
+        fi;
+        cp -r "$home$1" "$(dirname "$1")/";
 }
 
 [ -d $home/system/etc/firmware ] && {
         ui_print "- Copying firmware to $SYSTEM/etc/firmware"
-	install "/system/etc/firmware" 0755 0644 "$SYSTEM/etc/firmware";
+        install "/system/etc/firmware" 0755 0644 "$SYSTEM/etc/firmware";
 }
 
 [ -d $home/system/etc/init.d ] && {
         ui_print "- Copying init.d scripts to $SYSTEM/etc/init.d"
-	install "/system/etc/init.d" 0755 0755 "$SYSTEM/etc/init.d";
+        install "/system/etc/init.d" 0755 0755 "$SYSTEM/etc/init.d";
 }
 
 [ -d $home/system/lib ] && {
         ui_print "- Copying 32-bit shared libraries to ${SYSTEM}/lib"
-	install "/system/lib" 0755 0644 "$SYSTEM/lib";
+        install "/system/lib" 0755 0644 "$SYSTEM/lib";
 }
 
 [ -d $home/system/lib64 ] && {
         ui_print "- Copying 64-bit shared libraries to ${SYSTEM}/lib"
-	install "/system/lib64" 0755 0644 "$SYSTEM/lib64";
+        install "/system/lib64" 0755 0644 "$SYSTEM/lib64";
 }
 
 [ -d $home/system/bin ] && {
         ui_print "- Installing ${SYSTEM}/bin binaries"
-	install "/system/bin" 0755 0755 "$SYSTEM/bin";
+        install "/system/bin" 0755 0755 "$SYSTEM/bin";
 }
 
 [ -d $home/system/xbin ] && {
         ui_print "- Installing ${SYSTEM}/xbin binaries"
-	install "/system/xbin" 0755 0755 "$SYSTEM/xbin";
+        install "/system/xbin" 0755 0755 "$SYSTEM/xbin";
 }
 
 [ -d $home/data/local ] && {
         ui_print "- Copying additional files to /data/local"
-	install "/data/local" 0755 0644;
+        install "/data/local" 0755 0644;
 }
 [ -d $home/vendor/etc/init ] && {
         mount /vendor;
         chmod 644 $home/vendor/etc/init/*;
-	cp -r $home/vendor/etc/init/* /vendor/etc/init/;
+        cp -r $home/vendor/etc/init/* /vendor/etc/init/;
 }
 [ -d $home/ramdisk-patch ] && {
-	setperm "0755" "0750" "$home/ramdisk-patch";
+        setperm "0755" "0750" "$home/ramdisk-patch";
         chown root:shell $home/ramdisk-patch/*;
-	cp -rp $home/ramdisk-patch/* "$SYSTEM_ROOT/";
+        cp -rp $home/ramdisk-patch/* "$SYSTEM_ROOT/";
 }
 
 if [ ! "$(grep /init.nethunter.rc $SYSTEM_ROOT/init.rc)" ]; then
@@ -120,16 +107,90 @@ done;
 
 ## End NetHunter additions
 
-
-## AnyKernel file attributes
-##set permissions/ownership for included ramdisk files
+### AnyKernel install
+# begin attributes
+attributes() {
 set_perm_recursive 0 0 755 644 $ramdisk/*;
 set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+} # end attributes
 
 
-## AnyKernel install
-dump_boot;
+## boot shell variables
+block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+patch_vbmeta_flag=auto;
 
-write_boot;
-## end install
+# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
+. tools/ak3-core.sh && attributes;
+
+# boot install
+dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
+
+# init.rc
+backup_file init.rc;
+replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
+
+# init.tuna.rc
+backup_file init.tuna.rc;
+insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
+append_file init.tuna.rc "bootscript" init.tuna;
+
+# fstab.tuna
+backup_file fstab.tuna;
+patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
+patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
+patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
+append_file fstab.tuna "usbdisk" fstab;
+
+write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
+## end boot install
+
+
+## init_boot shell variables
+#block=init_boot;
+#is_slot_device=1;
+#ramdisk_compression=auto;
+#patch_vbmeta_flag=auto;
+
+# reset for init_boot patching
+#reset_ak;
+
+# init_boot install
+#dump_boot; # unpack ramdisk since it is the new first stage init ramdisk where overlay.d must go
+
+#write_boot;
+## end init_boot install
+
+
+## vendor_kernel_boot shell variables
+#block=vendor_kernel_boot;
+#is_slot_device=1;
+#ramdisk_compression=auto;
+#patch_vbmeta_flag=auto;
+
+# reset for vendor_kernel_boot patching
+#reset_ak;
+
+# vendor_kernel_boot install
+#split_boot; # skip unpack/repack ramdisk, e.g. for dtb on devices with hdr v4 and vendor_kernel_boot
+
+#flash_boot;
+## end vendor_kernel_boot install
+
+
+## vendor_boot shell variables
+#block=vendor_boot;
+#is_slot_device=1;
+#ramdisk_compression=auto;
+#patch_vbmeta_flag=auto;
+
+# reset for vendor_boot patching
+#reset_ak;
+
+# vendor_boot install
+#dump_boot; # use split_boot to skip ramdisk unpack, e.g. for dtb on devices with hdr v4 but no vendor_kernel_boot
+
+#write_boot; # use flash_boot to skip ramdisk repack, e.g. for dtb on devices with hdr v4 but no vendor_kernel_boot
+## end vendor_boot install
 
