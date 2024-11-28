677d112189c3c7732607d18799040999a9fc5529
g0t mi1k <have.you.g0tmi1k@gmail.com>
2024-11-28 14:51:34 +0000

    WIP: Magisk CLI support
    $ adb push *nethunter*.zip /sdcard/Download/nh.zip
    $ adb shell 'su -c magisk --install-module /sdcard/Download/nh.zip'

---
+++ nethunter/customize.sh
@@ -0,0 +1,11 @@
+## $ adb push *nethunter*.zip /sdcard/Download/nh.zip; adb shell 'su -c magisk --install-module /sdcard/Download/nh.zip'
+
+TMPDIR=/dev/tmp
+mkdir -p $TMPDIR
+cd $TMPDIR/
+
+echo "* Unpacking nethunter script"
+unzip -o "$ZIPFILE" META-INF/com/google/android/update-binary -d $TMPDIR
+
+echo "* Running nethunter script"
+[ -f $TMPDIR/META-INF/com/google/android/update-binary ] && sh $TMPDIR/META-INF/com/google/android/update-binary
