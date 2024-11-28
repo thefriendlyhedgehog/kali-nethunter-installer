679ffc0c3317edce2c893ac7d132a0410d7ae686
g0t mi1k <have.you.g0tmi1k@gmail.com>
2024-11-28 18:15:50 +0000

    TWRP & Magisk use set-wallpaper.sh + refresh logic

--- nethunter/tools/set-wallpaper.sh
+++ nethunter/tools/set-wallpaper.sh
@@ -1,39 +1,55 @@
 ## [Magisk & recovery/TWRP] [nethunter] [This is sourced, not a standalone script]
 ## Set the wallpaper based on device screen resolution
 
+[ -d $TMP/wallpaper ] || return 1
+
 ## Define wallpaper variables
 wp=/data/system/users/0/wallpaper
 wpinfo=${wp}_info.xml
 
-
-#Try to Grab the Wallpaper Height and Width from /sys 
-res_w=$(cat /sys/class/drm/*/modes | head -n 1 | cut -f1 -dx)
-res_h=$(cat /sys/class/drm/*/modes | head -n 1 | cut -f2 -dx)
-
-res="$res_w"x"$res_h" #Resolution Size
-
-#check if we grabbed Resolution from /sys or not
-if [ -z $res_h ] || [ -z $res_w ]; then
-  unset res res_h res_w
-
-  res=$(tools/screenres) #Try the old method for old devices
-
-  if [ ! "$res" ] || [[ "$res" == *"failed"* ]] || [ -f "wallpaper/resolution.txt" ]; then
-    if [ -f "wallpaper/resolution.txt" ]; then
-      res=$(cat wallpaper/resolution.txt)
-    else
-      print "! Can't get screen resolution from kernel! Skipping"
-      exit 1
-    fi
-  fi
-
-  res_w=$(echo "$res" | cut -f1 -dx)
-  res_h=$(echo "$res" | cut -f2 -dx)
+## If defined in https://gitlab.com/kalilinux/nethunter/build-scripts/kali-nethunter-kernels/-/blob/main/devices.yml (Manual)
+if [ -f "$TMP/wallpaper/resolution.txt" ]; then
+  echo "Method #1" >&2
+  res=$(cat $TMP/wallpaper/resolution.txt)
 fi
+
+## Check if we grabbed resolution from devices.yml
+[ -z $res ] && {
+  echo "Method #2" >&2
+
+  ## Get screen resolution using wm size
+  res=$(wm size | grep "Physical size:" | cut -d' ' -f3 2>/dev/null)
+}
+
+## Check if we grabbed resolution from wm or not
+[ -z $res ] && {
+  echo "Method #3" >&2
+
+  ## Try to grab the wallpaper height and width from /sys / sysfs
+  res="$(cat /sys/class/drm/*/modes | head -n 1)"
+}
+
+## Check if we grabbed resolution from /sys / sysfs or not
+[ -z $res ] && [ -f $TMP/tools/screenres ] && {
+  echo "Method #4" >&2
+
+  ## Try the old method for old devices
+  res=$($TMP/tools/screenres)
+
+  ([ -z "$res" ] || [[ "$res" == *"failed"* ]]) && unset res
+}
+
+## Give up...
+[ -z $res ] && {
+  print "! Can't get screen resolution of device! Skipping"
+  return 1
+}
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 print "- Found screen resolution: $res"
+res_w=$(echo "$res" | cut -f1 -dx)
+res_h=$(echo "$res" | cut -f2 -dx)
 
 if [ ! -f "wallpaper/$res.png" ]; then
   print "! No wallpaper found for your screen resolution. Skipping"
