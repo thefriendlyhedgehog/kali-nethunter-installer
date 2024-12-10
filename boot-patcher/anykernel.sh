cf7693cb2919be9a32186148e55e4cbfb0403098
Ben Wilson <g0tmi1k@kali.org>
2024-12-10 19:21:50 +0000

    Mark how scripts are being used

--- boot-patcher/anykernel.sh
+++ boot-patcher/anykernel.sh
@@ -1,8 +1,12 @@
+## [Magisk & recovery/TWRP] [boot-patcher] [This is standalone script, not sourced] - uses ash
+##
 ## AnyKernel3 Ramdisk Mod Script
 ##   Modified for NetHunter
 ##
 ## CREDITS to osm0sis @ xda-developers
 ##   REF: https://github.com/osm0sis/AnyKernel3/blob/ad53675d2e13e2b52adc706fb8492bfe5b76440b/anykernel.sh
+
+#set -x
 
 #------------------------------------------------------------------------------
 
